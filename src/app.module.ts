ba230e48a74badb7fe64f37615b1edcb6c937edc
ahadsenja <hamdanilm96@gmail.com>
2021-10-23 02:48:37 +0800

    chore: adding features module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { UsersController } from './features/users/users.controller';
+import { JournalsController } from './features/journals/journals.controller';
+import { TransactionsController } from './features/transactions/transactions.controller';
+
+import { UsersModule } from './features/users/users.module';
+import { JournalsModule } from './features/journals/journals.module';
+import { TransactionsModule } from './features/transactions/transactions.module';
+
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [TransactionsModule, UsersModule, JournalsModule],
+  controllers: [AppController, UsersController, JournalsController, TransactionsController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
