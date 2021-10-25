f3536b090073b515fadcc917cae88dd88ab27569
ahadsenja <hamdanilm96@gmail.com>
2021-10-26 05:54:13 +0800

    feat: create getAll, getById, and create method

--- src/features/users/users.controller.ts
+++ src/features/users/users.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, NotFoundException, Param, Post } from '@nestjs/common';
+
+import { User } from './users.entity';
+import { UserDTO } from './users.dto';
+import { UsersService } from './users.service';
+import { ObjectID } from 'mongodb';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+  constructor(
+    private readonly userService: UsersService
+  ) { }
+
+  @Get()
+  @HttpCode(200)
+  async getAll(): Promise<User[]> {
+    const result = await this.userService.findAll();
+    console.log(result)
+    return result;
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: string): Promise<User> {
+    const user = await this.userService.findOne(id);
+    console.log('ini adalah id dari user controller: ' + id);
+    return user;
+  }
+
+  @Post('/create')
+  async createUser(@Body() createUser: UserDTO): Promise<User> {
+    return await this.userService.create(createUser);
+  }
+
+}
