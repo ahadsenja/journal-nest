7d04754129a52f3e0a5f30af1a26abb1cf380de7
ahadsenja <hamdanilm96@gmail.com>
2021-10-26 06:59:27 +0800

    reupload users

--- src/features/users/users.dto.ts
+++
@@ -1,6 +0,0 @@
-export class UserDTO {
-  id: string;
-  name: string;
-  email: string;
-  password: string;
-}
--- src/features/users/users.entity.ts
+++
@@ -1,9 +0,0 @@
-import { Entity, ObjectID, ObjectIdColumn, Column } from 'typeorm';
-
-@Entity('users')
-export class User {
-  @ObjectIdColumn() id: ObjectID;
-  @Column() name: string;
-  @Column() email: string;
-  @Column() password: string;
-}
