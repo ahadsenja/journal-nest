19b7446078a364a314de7927ecf71c5d01f8d0e0
ahadsenja <hamdanilm96@gmail.com>
2021-10-26 06:58:25 +0800

    chore: update file import

--- src/features/users/users.module.ts
+++ src/features/users/users.module.ts
@@ -5,7 +5,7 @@ import { UsersController } from './users.controller';
 
 import { UsersService } from './users.service';
 
-import { User } from './users.entity';
+import { User } from '../../entity/users.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
