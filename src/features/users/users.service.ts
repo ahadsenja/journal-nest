b64c5a07b2614ef2b2506d9f9186a3e28ca5b4ac
ahadsenja <hamdanilm96@gmail.com>
2021-10-26 05:57:39 +0800

    feat: create findAll, getById, create method

--- src/features/users/users.service.ts
+++ src/features/users/users.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MongoRepository } from 'typeorm';
+import { ObjectID } from 'mongodb';
+
+import { User } from './users.entity';
+import { IUser } from './users.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: MongoRepository<User>
+  ) { }
+
+  async findAll(): Promise<User[]> {
+    const users = this.userRepository.find();
+    return users;
+  }
+
+  async findOne(id: string): Promise<User> {
+    const user = ObjectID.isValid(id) && (this.userRepository.findOne(id));
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    return user;
+  }
+
+  async create(createUser: IUser): Promise<User> {
+    const user = this.userRepository.save(createUser);
+    return user;
+  }
+}
