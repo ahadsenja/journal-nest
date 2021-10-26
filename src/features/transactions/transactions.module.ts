24865732e0298f1f913ee6b4713428f390e309af
ahadsenja <hamdanilm96@gmail.com>
2021-10-26 20:28:33 +0800

    chore: update transaction module

--- src/features/transactions/transactions.module.ts
+++ src/features/transactions/transactions.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TransactionsController } from './transactions.controller';
-
 import { TransactionsService } from './transactions.service';
+import { Transaction } from 'src/entity/transaction.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Transaction])],
   controllers: [TransactionsController],
   providers: [TransactionsService]
 })
