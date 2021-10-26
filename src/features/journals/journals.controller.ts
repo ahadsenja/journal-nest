624234315a4cee8935a91d7ba7eae08eac7e4eee
ahadsenja <hamdanilm96@gmail.com>
2021-10-26 20:27:16 +0800

    feat: create journal controller crud

--- src/features/journals/journals.controller.ts
+++ src/features/journals/journals.controller.ts
@@ -1,11 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { JournalDTO } from 'src/dto/journal.dto';
+import { Journal } from 'src/entity/journal.entity';
+import { IJournal } from 'src/interface/journal.interface';
+import { JournalsService } from './journals.service';
 
 @Controller('journals')
 export class JournalsController {
 
+  constructor(private journalService: JournalsService) { }
+
   @Get()
-  findAll() {
-    return 'This is journals endpoint';
+  async findAll(): Promise<Journal[]> {
+    const journals = await this.journalService.findAll();
+    return journals;
+  }
+
+  @Get(':id')
+  async GetById(@Param('id') id: string): Promise<Journal> {
+    const journal = await this.journalService.findById(id);
+    return journal;
+  }
+
+  @Post()
+  async createJournal(@Body() payload: JournalDTO): Promise<Journal> {
+    const journal = await this.journalService.create(payload);
+    return journal;
+  }
+
+  @Put(':id')
+  async updateJournal(@Param('id') id: string, @Body() payload: JournalDTO): Promise<Journal> {
+    const journal = await this.journalService.update(id, payload);
+    return journal;
+  }
+
+  @Delete(':id')
+  async deleteJournal(@Param('id') id: string): Promise<Journal> {
+    const journal = await this.journalService.delete(id);
+    return journal;
   }
 
 }
