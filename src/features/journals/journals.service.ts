4a10502d3ac5d0487fc8fef3d5dc0e702296e23d
ahadsenja <hamdanilm96@gmail.com>
2021-10-26 20:27:43 +0800

    feat: create journal crud service

--- src/features/journals/journals.service.ts
+++ src/features/journals/journals.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MongoRepository } from 'typeorm';
+import { ObjectId } from 'mongodb';
+
+import { Journal } from 'src/entity/journal.entity';
+import { JournalDTO } from 'src/dto/journal.dto';
+import { IJournal } from 'src/interface/journal.interface';
 
 @Injectable()
-export class JournalsService {}
+export class JournalsService {
+
+  constructor(
+    @InjectRepository(Journal)
+    private readonly journalRepository: MongoRepository<Journal>
+  ) { }
+
+  async findAll(): Promise<Journal[]> {
+    const journals = this.journalRepository.find();
+    return journals;
+  }
+
+  async findById(id: string): Promise<Journal> {
+    const journal = ObjectId.isValid(id) && this.journalRepository.findOne(id);
+    if (!journal) {
+      throw new NotFoundException('Data tidak ditemukan');
+    }
+    return journal;
+  }
+
+  async create(journalObject: IJournal): Promise<Journal> {
+    const journal = this.journalRepository.save(journalObject);
+    return journal;
+  }
+
+  async update(id: string, journalObject: IJournal): Promise<Journal> {
+    const journal = ObjectId.isValid(id) && this.journalRepository.findOne(id);
+    if (!journal) {
+      throw new NotFoundException('Data yang ingin di ubah tidak ditemukan');
+    }
+    this.journalRepository.update(id, journalObject);
+    return journal;
+  }
+
+  async delete(id: string): Promise<Journal> {
+    const journal = ObjectId.isValid(id) && this.journalRepository.findOne(id);
+    if (!journal) {
+      throw new NotFoundException('Data tidak ditemukan');
+    }
+    this.journalRepository.delete(id);
+    return journal;
+  }
+
+}
