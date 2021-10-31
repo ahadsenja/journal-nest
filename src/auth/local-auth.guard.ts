0f6636ba01e146c7a494340f53e7551cdd6aee5c
ahadsenja <hamdanilm96@gmail.com>
2021-10-31 18:12:11 +0800

    backud up

--- src/auth/local-auth.guard.ts
+++ src/auth/local-auth.guard.ts
@@ -3,10 +3,6 @@ import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
-  }
+export class LocalAuthGuard extends AuthGuard('local') {
+
 }
