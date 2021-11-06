fb452a300e6e64d4ff1c7be39b3fe1e5ec189664
ahadsenja <hamdanilm96@gmail.com>
2021-11-07 06:26:24 +0800

    feat: add register, login, and authenticated user

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,85 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Post, Req, Res, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { Request, Response } from 'express';
 
 import { AppService } from './app.service';
-import { LocalAuthGuard } from './auth/local-auth.guard';
+import { UsersService } from './features/users/users.service';
 
 @Controller()
 export class AppController {
 
-  constructor(private readonly appService: AppService) { }
+  constructor(
+    private readonly appService: AppService, 
+    private userService: UsersService,
+    private jwtService: JwtService
+  ) { }
 
-  @UseGuards(LocalAuthGuard)
+  // Register new user
+  @Post('auth/register')
+  async register(
+    @Body('name') name: string,
+    @Body('email') email: string,
+    @Body('username') username: string,
+    @Body('password') password: string
+  ){
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = await this.userService.create({
+      name,
+      email, 
+      username, 
+      password: hashedPassword
+    });
+    delete user.password; // For remove password when returning data
+    return user;
+  }
+
+  // Login user
   @Post('auth/login')
-  async login(@Request() req) {
-    return req.user;
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+    @Res({passthrough: true}) res: Response 
+  ) {
+    const user = await this.userService.findOneByCondition({email});
+    if (!user) {
+      throw new BadRequestException('Invalid credentials, User not found');
+    }
+
+    const passCompare = await bcrypt.compare(password, user.password);
+    if (!passCompare) {
+      throw new BadRequestException('Invalid credentials, Invalid email or password');
+    }
+
+    const jwt = await this.jwtService.signAsync({id: user.id});
+    res.cookie('jwt', jwt, {httpOnly: true});
+    return res.send({
+      message: "Login success",
+      "access_token": jwt
+    })
   }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // Check authenticated user
+  @Get('user')
+  async user(@Req() req: Request) {
+    try {
+      const cookie = req.cookies['jwt'];
+      const data = await this.jwtService.verifyAsync(cookie);
+      if (!data) {
+        throw new UnauthorizedException();
+      }
+      const user = await this.userService.findOneById(data['id']);
+      const {password, ...result} = user;
+      return result;
+    } catch (error) {
+      throw new UnauthorizedException();
+    }
+    
   }
+
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
 
 }
