be31583d571470a7ad6c16da6670975557e9c84f
ahadsenja <hamdanilm96@gmail.com>
2021-11-13 05:13:51 +0800

    chore: update user service

--- src/features/users/users.service.ts
+++ src/features/users/users.service.ts
@@ -1,10 +1,12 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, Res } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MongoRepository } from 'typeorm';
 import { ObjectID } from 'mongodb';
 
 import { User } from '../../entity/users.entity';
 import { IUser } from '../../interface/users.interface';
+import { Response } from 'express';
+import { UserDTO } from 'src/dto/users.dto';
 
 @Injectable()
 export class UsersService {
