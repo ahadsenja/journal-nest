3b7af57b92642176b45203c77d6e838659bc78b6
ahadsenja <hamdanilm96@gmail.com>
2021-11-13 05:10:55 +0800

    feat: add one to many relation to user table

--- src/entity/journal.entity.ts
+++ src/entity/journal.entity.ts
@@ -1,8 +1,15 @@
-import { Column, Entity, ObjectID, ObjectIdColumn } from "typeorm";
+import { Column, Entity, ManyToOne, ObjectID, ObjectIdColumn, OneToMany } from "typeorm";
+import { Transaction } from "./transaction.entity";
+import { User } from "./users.entity";
 
 @Entity('journals')
 export class Journal {
   @ObjectIdColumn() id: ObjectID;
   @Column() name: string;
   @Column() description: string;
+  @ManyToOne(() => User, user => user.journals)
+  user: User;
+  @OneToMany(() => Transaction, transaction => transaction.journals)
+  transactions: Transaction[];
+  @Column() userId: string;
 }
